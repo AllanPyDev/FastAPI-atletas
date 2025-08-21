43e2ccae2701bc0e35e977469b03eb2ffe6aa209
AllanPyDev <beckerallan925@gmail.com>
2025-08-21 10:25:29 -0300

    Update schemas.py

--- App/schemas.py
+++ App/schemas.py
@@ -1,1 +1,17 @@
+from pydantic import BaseModel
 
+class AtletaCreate(BaseModel):
+    """Schema para criação de atleta."""
+    nome: str
+    cpf: str
+    centro_treinamento: str | None = None
+    categoria: str | None = None
+
+class AtletaOut(BaseModel):
+    """Schema de saída de atleta."""
+    nome: str
+    centro_treinamento: str | None
+    categoria: str | None
+
+    class Config:
+        orm_mode = True
